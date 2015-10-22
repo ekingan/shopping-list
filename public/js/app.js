3c4499badebf1e2ae600d289f92bcde7744269ec
ekingan <ekingan@gmail.com>
2015-10-22 14:57:18 -0700

    Able to post to the DOM using jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -3,30 +3,66 @@
 $(document).ready(function(){
 	console.log('Hey, Earth!');
 
+
+//Event handler for entering new item 
   $('#new-item-form').on('submit', function (event){
   	event.preventDefault();
-  	var item = $(this).serialize();
-  	$('ul').prepend(item);
-  	$.ajax({
-  		url: '/items', 
-  		type: 'POST', 
-  		data: item
-  	})
-  		.done(function (data){
-  			console.log("you added an new item ", data);
+  	var item = $('#item-input').val();
+  	
+  	var itemHtml = "<li class=''>" + 
+  								"<div class='radio'>" +
+  								"<label class='radio-inline'>" + 
+  								"<input type='radio' value='' name='' data-id=''></label>"
+  									+ item + 
+  									"<progress max='100' value='50'>" +
+  									"<strong> 50% done.</strong>" +
+										"</progress>" +
+  	 								"<span data-id='" + //data._id 
+  	 								"' class='close delete'>x</span></li>"; 
+  	if (item !== ''){
 
-  			var itemHtml = "<li class=''>" + data.name + 
-  			"<span data-id='" + data._id + "' class='close delete'>x</span></li>"; 
+			$('#list-items-ul').prepend(itemHtml);
+			$('#new-item-form')[0].reset();
+				console.log("you added " + item);
+		} else {
+			console.log("your form is blank");
+		}
+	});
 
-  			$('#list-items').prepend(itemHtml);
-  			$('#new-item-form')[0].reset();
 
-  		})
-  			.fail(function (data){
-  				console.log("item was not added");
-  			});
+	//event handler for getting data from the dropdown
+
+	$('.dropdown-menu li').on('click', function () {
+		$(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+  	$(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+	});
+	
+
+  	//before serialize 
+  	//get value of bootstrap dropdown
+  	//save value into value of hidden form field
+  	// finally serialize the form
+
+
+
+  	
+  	
+  	
+  	// $.ajax({
+  	// 	url: '/items', 
+  	// 	type: 'POST', 
+  	// 	data: item
+  	// })
+  	// 	.done(function (data){
+  	// 		console.log("you added an new item ", data);
+  	
+
+  	// 	})
+  	// 		.fail(function (data){
+  	// 			console.log("item was not added");
+  	// 		});
   		
-  	});
+  	
 
 
 
